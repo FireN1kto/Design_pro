7ff1c68caf28b34a5274532f4eb5abf41e2eef96
F1reN1kto <maksimcymbal77@gmail.com>
2024-12-05 20:38:47 +0700

    Добавил страницы входа, выхода и профиль

--- AppDesign/urls.py
+++ AppDesign/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, login, profile, logout
 
 app_name = 'AppDesign'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('accounts/login', login.as_view(), name='login'),
+    path('accounts/logout/', logout.as_view(), name='logout'),
+    path('accounts/profile', profile, name='profile'),
 ]
--- AppDesign/views.py
+++ AppDesign/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+def index(request):
+    return render(request, 'catalog/index.html')
+
+class login(LoginView):
+    template_name = 'catalog/login.html'
+
+class logout(LoginRequiredMixin, LogoutView):
+    template_name = 'catalog/logout.html'
+
+@login_required
+def profile(request):
+    return render(request, 'catalog/profile.html')
+
